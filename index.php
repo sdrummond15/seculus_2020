b2d253c612c8c0e52dffe3cb2313c660b00632e4
Silvio <silvio@sdrummond.com.br>
2020-03-01 21:38:19 -0300

    Alterando Projeto

--- index.php
+++ index.php
@@ -1,543 +1,246 @@
 <!DOCTYPE html>
 
-
-
 <html lang="en">
 
-
-
 <head>
-
     <meta charset="UTF-8">
-
     <title>Seculus</title>
-
     <meta name="description" content="Show de Prêmios Seculus.">
-
     <meta name="author" content="SDrummond">
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <meta property="og:title" content="Show de Prêmios Seculus" />
-
     <meta property="og:description" content="Show de Prêmios Seculus.">
-
     <meta property="og:image:type" content="image/jpeg">
-
     <meta property="og:image:width" content="450">
-
-    <link rel="icon" type="image/png" href="images/favicon.png"/>
-
+    <link rel="icon" type="image/png" href="images/favicon.png" />
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-
     <link rel="stylesheet" type="text/css" href="assets/css/fontawesome-all.min.css">
-
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-24132529-60"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-24132529-60"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-  gtag('config', 'UA-24132529-60');
-</script>
-
-
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'UA-24132529-60');
+    </script>
 </head>
 
-
-
 <body>
-
-<div id="topo"></div>
-
-<header id="header">
-
-
-
-    <div id="menu">
-
-
-
-        <div class="menu">
-
-            <div class="main-menu">
-
-                <ul>
-
+    <div id="topo"></div>
+    <header id="header">
+        <div id="menu">
+            <div class="menu">
+                <div class="main-menu">
+                    <ul>
+                        <li><a href="#periodo">Como Participar</a></li>
+                        <li><a href="#sorteio">Sorteios e Prêmios</a></li>
+                        <li><a href="resultados.php">Números da Sorte</a></li>
+                        <li><a href="#ganhadores">Resultados</a></li>
+                        <li><a href="#contato">Fale Conosco</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="menuresp">
+                <button title="button">
+                    <i class="fas fa-bars"></i>
+                </button>
+                <ul class="responsive">
                     <li><a href="#periodo">Como Participar</a></li>
-
                     <li><a href="#sorteio">Sorteios e Prêmios</a></li>
-
                     <li><a href="resultados.php">Números da Sorte</a></li>
-
                     <li><a href="#ganhadores">Resultados</a></li>
-
                     <li><a href="#contato">Fale Conosco</a></li>
-
-
-
                 </ul>
-
             </div>
-
         </div>
-
-
-
-        <div id="menuresp">
-
-            <button title="button">
-
-                <i class="fas fa-bars"></i>
-
-            </button>
-
-            <ul class="responsive">
-
-                <li><a href="#periodo">Como Participar</a></li>
-
-                <li><a href="#sorteio">Sorteios e Prêmios</a></li>
-
-                <li><a href="resultados.php">Números da Sorte</a></li>
-
-                <li><a href="#ganhadores">Resultados</a></li>
-
-                <li><a href="#contato">Fale Conosco</a></li>
-
-
-
-            </ul>
-
+        <div id="slide">
+            <img src="images/premios_seculus.png" alt="promocao seculus" />
         </div>
-
-
-
-    </div>
-
-
-
-    <div id="slide">
-
-        <img src="images/premios_seculus.png" alt="promocao seculus"/>
-
-    </div>
-
-
-
-</header>
-
-
-
-
-
-<section id="marcas">
-
-
-
-    <div class="marcas">
-
-
-
-        <article>
-
-
-
-            <ul>
-
-                <li>
-
-                    <img src="images/marcas/seculus.png" alt="seculus"/>
-
-                </li>
-
-                <li>
-
-                    <img src="images/marcas/mondaine.png" alt="mondaine"/>
-
-                </li>
-
-                <li>
-
-                    <img src="images/marcas/speedo.png" alt="speedo"/>
-
-                </li>
-
-                <li>
-
-                    <img src="images/marcas/guess.png" alt="guess"/>
-
-                </li>
-
-                <li>
-
-                    <img src="images/marcas/cassio.png" alt="cassio"/>
-
-                </li>
-
-            </ul>
-
-
-
-        </article>
-
-
-
-    </div>
-
-
-
-</section>
-
-
-
-<section id="periodo">
-
-
-
-    <div class="periodo">
-
-
-
-        <article>
-
-
-
-            <h1>PERÍODO: DE 01/10 A 30/11/2019</h1>
-
-
-
-            <div class="linha"></div>
-
-
-
-            <h2>PARA PARTICIPAR:</h2>
-
-
-
-            <div id="participar">
-
-
-
+    </header>
+    <section id="marcas">
+        <div class="marcas">
+            <article>
                 <ul>
-
                     <li>
-
-                        <img src="images/inscricao.png" alt="inscricao" />
-
-                        <span>
-
-                            A cada R$ 3.000,00 por pedido implementado, você estará apto a concorrer.
-
-                        </span>
-
+                        <img src="images/marcas/seculus.png" alt="seculus" />
                     </li>
-
-                    <li class="next">
-
-                        <img src="images/next.png" alt="next" />
-
-                    </li>
-
                     <li>
-
-                        <img src="images/computador.png" alt="como fazer" />
-
-                        <span>
-
-                            Sistema da Promoção gera um ou mais números da sorte a partir do valor líquido do pedido.
-
-                        </span>
-
+                        <img src="images/marcas/mondaine.png" alt="mondaine" />
                     </li>
-
-                    <li class="next">
-
-                        <img src="images/next.png" alt="next" />
-
-                    </li>
-
                     <li>
-
-                        <img src="images/email.png" alt="confirmacao" />
-
-                        <span>
-
-                            Voce receberá seu(s) números(s) da sorte por e-mail.
-
-                        </span>
-
+                        <img src="images/marcas/speedo.png" alt="speedo" />
                     </li>
-
+                    <li>
+                        <img src="images/marcas/guess.png" alt="guess" />
+                    </li>
+                    <li>
+                        <img src="images/marcas/cassio.png" alt="cassio" />
+                    </li>
                 </ul>
-
-
-
-            </div>
-
-
-
-            <div class="linha"></div>
-
-
-
-        </article>
-
-
-
-
-
-
-
-    </div>
-
-
-
-</section>
-
-
-
-<section id="sorteio">
-
-
-
-    <div class="sorteio">
-
-
-
-        <article>
-
-
-
-            <div class="sorteio-semana">
-
-                <h2>Sorteio com prêmios toda semana!</h2>
-
-                <img src="images/sorteio_eletronicos.png" alt="sorteio eletronicos" />
-
-                <div class="box-info">
-
-                    <h3>19 e 26 de Outubro</h3>
-
-                    <h3>02*, 09, 16, 23 e 30 de Novembro</h3>
-
-                    <ul>
-
-                        <li>01 Smartphone</li>
-
-                        <li>01 SmarTV</li>
-
-                        <li>01 Caixa de som portátil</li>
-
-                    </ul>
-
+            </article>
+        </div>
+    </section>
+    <section id="periodo">
+        <div class="periodo">
+            <article>
+                <h2>Período da promoção:</h2>
+                <h1>De 09 de março a 31 de junho.</h1>
+                <div class="linha"></div>
+                <h2>Para participar é muito simples!</h2>
+                <h1>A cada R$ 3.000,00 por pedido individual implementado, o cliente recebe um número da sorte.</h1>
+            </article>
+        </div>
+    </section>
+    <section id="sorteio-nacional">
+        <div class="sorteio">
+            <h1>SORTEIOS NACIONAIS</h1>
+            <article>
+                <div class="data-sorteio-1">
+                    <p>Dia 29 de Abril</p>
                 </div>
-
-            </div>
-
-
-
-            <div class="sorteio-nacional">
-
-                <h2>Sorteio Nacionais</h2>
-
-                <div class="box-info">
-
-                    <h3>02* de Novembro</h3>
-
-                    <ul>
-
-                        <li>01 Moto Honda</li>
-
-                        <li>01 Vale-Viagem de R$8.000,00</li>
-
-                        <li>01 Iphone</li>
-
-                        <li>01 Ipad</li>
-
-                    </ul>
-
+                <div class="data-sorteio-2">
+                    <p>Dia 03 de Junho</p>
                 </div>
-
-                <div class="imagem-sorteio">
-
-                    <img src="images/sorteio_nacional.png" alt="sorteio nacional" />
-
-                </div>
-
-                <div class="box-info">
-
-                    <h3>07 de Dezembro</h3>
-
-                    <ul>
-
-                        <li>01 Jeep Renegate</li>
-
-                        <li>01 Moto Honda</li>
-
-                        <li>01 Vale-Viagem de R$8.000,00</li>
-
-                        <li>01 Iphone</li>
-
-                        <li>01 Ipad</li>
-
-                    </ul>
-
-                </div>
-		<span>*Conforme previsto em regulamento, sorteio adiado para o dia 06/11, devido ao feriado nacional.</span>
-
-            </div>
-
-
-
-            <div class="linha"></div>
-
-
-
-        </article>
-
-
-
-    </div>
-
-
-
-</section>
-
-
-
-<section id="ganhadores">
-
-
-
-    <div class="ganhadores">
-
-
-
-        <article>
-
-
-
-            <h1>CONFIRA AQUI OS GANHADORES DOS SORTEIOS</h1>
-
-
-
-            <ul>
-
-                <li>
-                    <a href="sorteios.php?sorteio=regional" class="btn-ganhadores" id="ganhadores-regionais">SORTEIOS REGIONAIS</a>
-                </li>
-                <li>
-                    <a href="sorteios.php?sorteio=nacional" class="btn-ganhadores" id="ganhadores-nacionais">SORTEIOS NACIONAIS</a>
-                </li>
-            </ul>
-
-            <div class="linha"></div>
-
-            <div id="resultado-ganhadores">
-
-
-
-            </div>
-
-
-
-
-
-        </article>
-
-
-
-    </div>
-
-
-
-</section>
-
-<section id="contato">
-
-
-
-    <div class="contato">
-
-
-
-        <article>
-
-
-
-            <h3>Fale Conosco:</h3>
-
-
-
-            <div id="retornoHTML">
-
-                <form>
-
-                    <fieldset>
-
-                        <input id="nome" type="text" placeholder="Nome"><br>
-
-                        <input id="email" type="email" placeholder="E-mail"><br>
-
-                        <input id="phone" type="tel" placeholder="Telefone"><br>
-
-                        <textarea id="msg" placeholder="Mensagem:"></textarea><br>
-
-                        <input type="submit" id="btn" class="btn-contato" value="Enviar">
-
-                        <div class="loading">
-
-                            <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
-
+                <div class="container-sorteio">
+                    <div class="argo">
+                        <p><b>2</b> Fiat Argo</p>
+                    </div>
+                    <div class="img-sorteio">
+                        <img src="images/sorteio_nacional.png" alt="sorteio eletronicos" />
+                    </div>
+                    <div class="box-premios-nacionais">
+                        <div class="sorteio-mais">
+                            <p>E mais...</p>
                         </div>
-
-                    </fieldset>
-
-                </form>
-
-
-
-            </div>
-
-
-
-        </article>
-
-
-
-        <div class="linha"></div>
-
-
-
-    </div>
-
-
-
-</section>
-
-
-
-<footer>
-
+                        <ul>
+                            <li>
+                                <div class="img-premio-nacional" style="background-image: url('images/carro.png');"></div>
+                                <span>2 Iphone 11</span>
+                            </li>
+                            <li>
+                                <div class="img-premio-nacional" style="background-image: url('images/carro.png');"></div>
+                                <span>2 Motos Honda</span>
+                            </li>
+                            <li>
+                                <div class="img-premio-nacional" style="background-image: url('images/carro.png');"></div>
+                                <span>2 Notebooks</span>
+                            </li>
+                            <li>
+                                <div class="img-premio-nacional" style="background-image: url('images/carro.png');"></div>
+                                <span>2 Caixas JBS Party</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </section>
+    <section id="sorteio-quinzenal">
+        <div class="sorteio">
+            <h1>SORTEIOS QUINZENAIS</h1>
+            <article>
+                <div class="data-sorteio-quinzenal-1">
+                    <p>DIA 21 DE MARÇO</p>
+                </div>
+                <div class="data-sorteio-quinzenal-2">
+                    <p>DIAS 04 E 18 DE ABRIL</p>
+                </div>
+                <div class="data-sorteio-quinzenal-3">
+                    <p>DIAS 02, 16 E 30 DE MAIO</p>
+                </div>
+                <div class="container-sorteio">
+                    <div class="argo">
+                        <p><b>2</b> Fiat Argo</p>
+                    </div>
+                    <div class="img-sorteio">
+                        <img src="images/sorteio_nacional.png" alt="sorteio eletronicos" />
+                    </div>
+                    <div class="box-premios-quinzenais">
+                        <ul>
+                            <li>
+                                <div class="img-premio-quinzenal" style="background-image: url('images/carro.png');"></div>
+                                <span>30 Tablets</span>
+                            </li>
+                            <li>
+                                <div class="img-premio-quinzenal" style="background-image: url('images/carro.png');"></div>
+                                <span>30 SmartTVs 32"</span>
+                            </li>
+                            <li>
+                                <div class="img-premio-quinzenal" style="background-image: url('images/carro.png');"></div>
+                                <span>30 Purificadores de água</span>
+                            </li>
+                            <li>
+                                <div class="img-premio-quinzenal" style="background-image: url('images/carro.png');"></div>
+                                <span>30 Smartphones</span>
+                            </li>
+                            <li>
+                                <div class="img-premio-quinzenal" style="background-image: url('images/carro.png');"></div>
+                                <span>30 Aspiradores de pó robô</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </section>
+    <section id="ganhadores">
+        <div class="ganhadores">
+            <article>
+                <h1>CONFIRA OS GANHADORES DA PROMOÇÃO</h1>
+                <ul>
+                    <li>
+                        <a href="sorteios.php?sorteio=regional" class="btn-ganhadores" id="ganhadores-regionais">SORTEIOS REGIONAIS</a>
+                    </li>
+                    <li>
+                        <a href="sorteios.php?sorteio=nacional" class="btn-ganhadores" id="ganhadores-nacionais">SORTEIOS NACIONAIS</a>
+                    </li>
+                </ul>
+                <div id="resultado-ganhadores">
+                </div>
+            </article>
+        </div>
+    </section>
+    <section id="contato">
+        <div class="contato">
+            <article>
+                <h3>Fale Conosco</h3>
+                <div id="retornoHTML">
+                    <form>
+                        <fieldset>
+                            <input id="nome" type="text" placeholder="Nome"><br>
+                            <input id="email" type="email" placeholder="E-mail"><br>
+                            <input id="phone" type="tel" placeholder="Telefone"><br>
+                            <textarea id="msg" placeholder="Mensagem:"></textarea><br>
+                            <input type="submit" id="btn" class="btn-contato" value="Enviar">
+                            <div class="loading">
+                                <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+            </article>
+        </div>
+    </section>
+    <footer>
         <p>&copy; 2019 por Seculus da Amazônia S/A
-
             Promoção interna válida para parceiros comerciais da Seculus da Amazônia, não aberta ao público em geral. Consulte regras de participação.</p>
-
-</footer>
-
+    </footer>
 
 
-<span class="topo">
 
-    <a href="#topo">
+    <span class="topo">
 
-        <i class="fas fa-angle-up"></i>
+        <a href="#topo">
 
-    </a>
+            <i class="fas fa-angle-up"></i>
 
-</span>
+        </a>
+
+    </span>
 
 
 
